b660556cea019eb274aef159fcd50083b2a1f38b
bansalshruti69 <bansalshruti69@gmail.com>
2020-06-02 13:09:08 +0530

    After visual polish

--- app/src/main/java/com/example/android/music/MainActivity.java
+++ app/src/main/java/com/example/android/music/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.android.music;
 
 import android.media.MediaPlayer;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
 
-    FragmentPagerAdapter adapterViewPager;
+    private TabLayout tabLayout;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
-        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
-        vpPager.setAdapter(adapterViewPager);
+
+        //Adding toolbar to the activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("PARTY"));
+        tabLayout.addTab(tabLayout.newTab().setText("SAD"));
+        tabLayout.addTab(tabLayout.newTab().setText("ROMANTIC"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
     }
 }
--- app/src/main/java/com/example/android/music/MyPagerAdapter.java
+++ app/src/main/java/com/example/android/music/MyPagerAdapter.java
@@ -5,45 +5,24 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class MyPagerAdapter extends FragmentPagerAdapter {
-    private static int NUM_ITEMS = 3;
-
-    public MyPagerAdapter(FragmentManager fragmentManager) {
-        super(fragmentManager);
+    int tabCount;
+    public MyPagerAdapter(FragmentManager fm, int tabcount) {
+        super(fm);
+        tabCount = tabcount;
     }
 
-    // Returns total number of pages.
-    @Override
-    public int getCount() {
-        return NUM_ITEMS;
-    }
-
-    // Returns the fragment to display for a particular page.
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return new fragment_party();
-            case 1:
-                return new fragment_sad();
-            case 2:
-                return new fragment_romatic();
-            default:
-                return null;
-        }
-
+        if (position == 0) {
+            return new fragment_party();
+        } else if (position == 1){
+            return new fragment_sad();
+        } else
+            return new fragment_romatic();
     }
 
     @Override
-    public CharSequence getPageTitle(int position) {
-        switch (position) {
-            case 0:
-                return "Party";
-            case 1:
-                return "Sad";
-            case 2:
-                return "Romantic";
-            default:
-                return "";
-        }
+    public int getCount() {
+        return 3;
     }
 }
